5ebab8ffdf8bfb5e545c44f2ecc5a42aef17ebe3
Firuzə Poladzadə <pfiruza.p202@code.edu.az>
2023-01-08 20:22:25 +0400

    wips

---
+++ Minidea/ViewModels/BlogCategoryVIewModel.cs
@@ -0,0 +1,23 @@
+﻿using Minidea.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Minidea.ViewModels
+{
+    public class BlogCategoryVIewModel
+    {
+        public int Id { get; set; }
+        public int? CategoryId { get; set; }
+        public int? UserId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? BigTitle { get; set; }
+        public string? SubTitle { get; set; }
+        public string? Text { get; set; }
+        public DateTime Date { get; set; }
+        public string PhotoURL { get; set; }
+        public BlogsCategories? Category { get; set; }
+        public AppUser? User { get; set; }
+
+        [NotMapped]
+        public IFormFile Photo { get; set; }
+    }
+}
