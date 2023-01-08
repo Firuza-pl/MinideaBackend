0b72646f9eee9db013e2c6443f15c6e0b8606166
Firuzə Poladzadə <pfiruza.p202@code.edu.az>
2023-01-08 16:45:55 +0400

    wips

--- Minidea/Areas/Admin/Controllers/PlaceController.cs
+++ Minidea/Areas/Admin/Controllers/PlaceController.cs
@@ -50,7 +50,6 @@ namespace Minidea.Areas.Admin.Controllers
             return View();
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(PlacePhotoVIewModel placePhoto)
@@ -90,7 +89,7 @@ namespace Minidea.Areas.Admin.Controllers
             }
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("Places");
+            return RedirectToAction("Edit", new { id = placePhoto.AdvertismentPlaceId });
         }
 
         [ActionName("Edit")]
@@ -154,13 +153,180 @@ namespace Minidea.Areas.Admin.Controllers
             newBackgroundImages.IsMain = true;
             newBackgroundImages.AreaTitle = photoLineViewModel.AreaTitle;
 
-            AdvertismentPlace advertismentPlace =  _context.AdvertismentPlaces.Find(newBackgroundImages.AdvertismentPlaceId);
+            AdvertismentPlace advertismentPlace = _context.AdvertismentPlaces.Find(newBackgroundImages.AdvertismentPlaceId);
 
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Edit), new { id = advertismentPlace.Id });
         }
 
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
 
+            if (id == null) return View("Error");
+
+            AdvertismentPhoto? data = await _context.AdvertismentPhotos.FindAsync(id);
+
+
+            if (data == null) return View("Error");
+            ViewBag.Active = "Home";
+
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(AdvertismentPhoto advertismentPhoto)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            AdvertismentPhoto? data = await _context.AdvertismentPhotos.FindAsync(advertismentPhoto.Id);
+
+
+#pragma warning disable CS8604 // Possible null reference argument.
+            string computerPhoto = Path.Combine(_env.WebRootPath, "img", data.PhotoURL);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            if (System.IO.File.Exists(computerPhoto))
+            {
+                System.IO.File.Delete(computerPhoto);
+            }
+
+            _context.AdvertismentPhotos.RemoveRange(advertismentPhoto);
+
+            await _context.SaveChangesAsync();
+            ViewBag.Active = "Home";
+
+            return RedirectToAction(nameof(Places));
+        }
+
+        //SİNGLE PLACE
+        public IActionResult CreateSinglePlace(int? id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            ViewBag.Active = "Home";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateSinglePlace(AdvertismentPlace advertismentPlace)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            ViewBag.Active = "Home";
+            AdvertismentPlace place = new AdvertismentPlace
+            {
+                BigTitle = advertismentPlace.BigTitle,
+                IsActive = advertismentPlace.IsActive = true
+            };
+
+            await _context.AdvertismentPlaces.AddAsync(place);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Places));
+        }
+
+        [ActionName("EditSinglePlace")]
+        public IActionResult EditSinglePlace(int? id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (id == null) return View("Error");
+            AdvertismentPlace? data = _context.AdvertismentPlaces.Find(id);
+
+            return View(data);
+        }
+
+        [ActionName("EditSinglePlace")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditSinglePlace(AdvertismentPlace advertismentPlace)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            AdvertismentPlace? place = await _context.AdvertismentPlaces.FindAsync(advertismentPlace.Id);
+
+            if (place == null) return View("Error");
+
+            advertismentPlace.BigTitle = place.BigTitle;
+            advertismentPlace.IsActive = true;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Places));
+        }
+
+        [ActionName("DeleteSinglePlace")]
+        public async Task<IActionResult> DeleteSinglePlace(int? id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (id == null) return View("Error");
+
+            AdvertismentPlace? data = await _context.AdvertismentPlaces.FindAsync(id);
+
+
+            if (data == null) return View("Error");
+            ViewBag.Active = "Home";
+
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("DeleteSinglePlace")]
+        public async Task<IActionResult> DeleteSinglePlace(AdvertismentPlace advertismentPlace)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            AdvertismentPlace? staticData = await _context.AdvertismentPlaces.FindAsync(advertismentPlace.Id);
+
+            IEnumerable<AdvertismentPhoto> advertismentPhoto = _context.AdvertismentPhotos.Where(p => p.AdvertismentPlaceId == advertismentPlace.Id).ToList();
+
+            foreach (var photo in advertismentPhoto)
+            {
+#pragma warning disable CS8604 // Possible null reference argument.
+                string computerPhoto = Path.Combine(_env.WebRootPath, "img", photo.PhotoURL);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+                if (System.IO.File.Exists(computerPhoto))
+                {
+                    System.IO.File.Delete(computerPhoto);
+                }
+
+                _context.AdvertismentPhotos.RemoveRange(advertismentPhoto);
+            }
+
+            _context.AdvertismentPlaces.Remove(staticData);
+
+            await _context.SaveChangesAsync();
+            ViewBag.Active = "Home";
+
+            return RedirectToAction(nameof(Places));
+        }
     }
 }
--- Minidea/wwwroot/admin/assets/js/dashboard.js
+++ Minidea/wwwroot/admin/assets/js/dashboard.js
@@ -1728,16 +1728,16 @@
       $('.hr-banner').hide();
     });
     if ($.cookie('plus-free-banner')!="true") {
-      document.querySelector('#proBanner').classList.add('d-flex');
+     // document.querySelector('#proBanner').classList.add('d-flex');
     }
     else {
       document.querySelector('#proBanner').classList.add('d-none');
     }
-    document.querySelector('#bannerClose').addEventListener('click',function() {
-      document.querySelector('#proBanner').classList.add('d-none');
-      document.querySelector('#proBanner').classList.remove('d-flex');
-      var date = new Date();
-      date.setTime(date.getTime() + 24 * 60 * 60 * 1000); 
-      $.cookie('plus-free-banner', "true", { expires: date });
-    });
+    //document.querySelector('#bannerClose').addEventListener('click',function() {
+    //  document.querySelector('#proBanner').classList.add('d-none');
+    //  document.querySelector('#proBanner').classList.remove('d-flex');
+    //  var date = new Date();
+    //  date.setTime(date.getTime() + 24 * 60 * 60 * 1000); 
+    //  $.cookie('plus-free-banner', "true", { expires: date });
+    //});
 })(jQuery);
