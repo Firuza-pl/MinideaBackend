d1f025fc418ae52c284e50553a72077e1a30562e
Firuzə Poladzadə <pfiruza.p202@code.edu.az>
2023-01-23 16:21:05 +0400

    wips

---
+++ Minidea/wwwroot/js/myGoogleMap.js
@@ -0,0 +1,470 @@
+﻿$(function () {
+    var lat = 40.3787576;
+    var lng = 49.87563599101423;
+
+    function initMap() {
+        var myLatLng = { lat: lat, lng: lng };
+
+        var styledMapType = new google.maps.StyledMapType([
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.country",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.locality",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.neighborhood",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#e6e6e6"
+                    },
+                    {
+                        "lightness": "35"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.landcover",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.landcover",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    },
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.terrain",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.attraction",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.business",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.government",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.medical",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.medical",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.medical",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.medical",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.medical",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.place_of_worship",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.school",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.school",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.school",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.school",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.sports_complex",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    },
+                    {
+                        "color": "#b7b7b7"
+                    },
+                    {
+                        "weight": "0.70"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "saturation": "0"
+                    },
+                    {
+                        "lightness": "-16"
+                    },
+                    {
+                        "gamma": "1.00"
+                    },
+                    {
+                        "weight": "0.01"
+                    },
+                    {
+                        "color": "#c9c0c0"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#52bfeb"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#2bcdfc"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            }
+        ], { name: 'Styled Map' });
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 15.2,
+            disableDefaultUI: true,
+            mapTypeControlOptions: {
+                mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+            }
+        });
+        map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
+
+        var myLatLng = { lat: lat, lng: lng };
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
---
+++ Minidea/wwwroot/js/myScroll.js
@@ -0,0 +1,101 @@
+﻿$(function () {
+
+
+    $("#next-ads").on('click', function () {
+        $('#carouselExampleControls4').carousel('next');
+    });
+    $("#prev-ads").on('click', function () {
+        $('#carouselExampleControls4').carousel('prev');
+    });
+    window.FakeLoader.init({
+        auto_hide: true
+    });
+
+    //fill carusel when open
+    $('#exampleModal').on('shown.bs.modal', function (e) {
+        var id = $(e.relatedTarget).attr('id');
+        try {
+            $.ajax({
+                url: "/Home/Load",
+                type: "get",
+                imageurl: String,
+                data: { id: id },
+                dataType: "json",
+                contentType: "application/json",
+                beforeSend: function () {
+                    $("#loading-image").show();
+                },
+                success: function (question) {
+                    $(JSON.parse(question)).each(function (index, item) {
+                        $('#carrusel').append($('<div class="carousel-item ads-background"><div style="height:450px;"><img class="img-fluid d-block w-100" src=/img/' + item.PhotoURL + '></div><p class="item-intro text-muted">' + item.AreaTitle + '</p></div>'));
+                        $('#carrusel .carousel-item').first().addClass('active');
+                        $("#loading-image").hide();
+                        $('#exampleModal').addClass('backColor');
+                    });
+                },
+                timeout: 15000,
+
+                error: function () {
+                    alert("error");
+                }
+            });
+        }
+        catch (e) {
+            alert(e.message);
+        }
+    });
+
+    //clear carusel after close
+    $("#exampleModal").on("hidden.bs.modal", function () {
+        $('#carrusel').empty();
+    });
+
+    $(window).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+
+        if ($(this).scrollTop() > $nav.height()) {
+            $('.navbar .navbar-brand img').attr('src', '/img/staticdata/logo-transparent.png');
+            $('.navbar .navbar-brand img').css('transition', 'opacity 1s ease-in-out');
+        }
+        if ($(this).scrollTop() < $nav.height()) {
+            $('.navbar .navbar-brand img').attr('src', '/img/staticdata/logo-coral.png');
+            $('.navbar .navbar-brand img').css('transition', 'opacity 1s ease-in-out');
+        }
+
+        var middleScreen = window.matchMedia("(min-width: 300px) and (max-width: 1200px");
+
+        if (middleScreen.matches) {
+            if ($(this).scrollTop() > $nav.height()) {
+                console.log("1");
+                $('.navbar-brand').css({
+                    'height': '80px',
+                    'padding-top': '27px'
+                });
+
+                $('.navbar-light .navbar-toggler').css({
+                    'color': 'rgb(0 0 0 / 35%)',
+                    'border-color': 'rgb(0 0 0 / 35%)',
+                    'margin-top' : '27px'
+                });
+             
+            }
+            if ($(this).scrollTop() < $nav.height()) {
+                console.log("2");
+
+                $('.navbar-brand').css({
+                    'height': '80px',
+                    'padding-top': '27px'
+                });
+
+                $('.navbar-light .navbar-toggler').css({
+                    'color': 'rgb(255 255 255 / 55%)',
+                    'border-color': 'rgb(255 255 255 / 55%)'
+                });
+              
+            }
+        }
+
+    })
+
+});
